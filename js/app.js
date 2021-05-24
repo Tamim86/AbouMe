a296a93de149380ab6e69903fbd5a36d09226b37
Tamim86 <tamimashour86@gmail.com>
2021-05-24 16:07:29 +0300

    question4

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
 'use strict'
-let favAnim = prompt('Do you like pets? ')
-let favFood = prompt('I don`t think we have good burger in Amman! Do you agree?')  
+let favAnim = prompt('Do you like pets? ');
+favFood.toLowerCase();
+let favFood = prompt('I don`t think we have good burger in Amman! Do you agree?');
+favFood.toLowerCase();  
 let trvel = prompt('I like traveling, I have visited many countries, would you like to join in my next trip?')
-let sport = prompt('Do you do sport often?')
-let shisha = prompt('I smoke shisha everyday, do you?!')
+trvel.toLowerCase();
+let sport = prompt('Do you do sport often?');
+sport.toLowerCase();
+let shisha = prompt('I smoke shisha everyday, do you?!');
+shisha.toLowerCase
 
 if (favAnim == 'yes'|| favAnim == 'y'){
 // console.log('Great I like them too!')
 alert('Cool I like them too!');
 }
-else if(favAnim== 'No'|| favAnim=='N'){
+else if(favAnim== 'no'|| favAnim=='n'){
     //console.log('Whats wrong with you?')
     alert('Whats wrong with you?');
     } else{
@@ -20,15 +25,19 @@ switch(favFood){
     case 'yes':
         // console.log('Isn`t that shameful!')
         alert('Isn`t that shameful!');
+        break;
     case 'y':
         // console.log('Isn`t that shameful!')
         alert('Isn`t that shameful!');
+        break;
     case 'no':
         //console.log('Prove it!');
         alert('Prove it!');
+        break;
     case 'n':
         //console.log('Prove it!');
         alert('Prove it!');
+        break;
     default:
         //console.log('I think you don`t like burgers!');
         alert('I think you don`t like burgers!')
@@ -39,15 +48,19 @@ switch(trvel){
     case 'yes':
         //console.log('Awsome, will be lots of fun')
         alert('Awsome, will be lots of fun');
+        break;
     case 'y':
         //console.log('Awsome, will be lots of fun')
         alert('Awsome, will be lots of fun');
+        break;
     case 'no':
         //console.log('Fine! will send you awsome photos')
         alert('Fine! will send you awsome photos');
+        break;
     case 'n':
          //console.log('Fine! will send you awsome photos')
          alert('Fine! will send you awsome photos');
+         break;
     default:
         //console.log('You have to do something in your vacation')
         alert('You have to do something in your vacation')
@@ -55,4 +68,15 @@ switch(trvel){
 
 }
 
+if (sport == 'yes'|| sport=='y'){
+    //console.log('Awsome, keep it up!')
+    alert('Awsome, keep it up!')
+}else if(sport=='no'|| sport=='n'){
+    //console.log('You are missing a lot')
+    alert('You are missing a lot');
+
+}else{
+    //console.log('Don`t run away from the answer')
+    alert('Don`t run away from the answer')
+}
 
